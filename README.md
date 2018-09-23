97acaa452358a3bc3005a224c3523d89bd29ee0e
Niklas Kreer <kreergit@posteo.de>
2018-09-23 19:12:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# i3mag
+
+A very simple screen magnification solution for use with [i3wm](https://i3wm.org/) that makes use of xrandr's panning option. And yes, it's a PHP script. So make sure you have that installed.
+
+## Getting started
+
+Installation of i3mag is easy. It can add itself to your i3 configuration automatically so you don't have to figure this stuff out. Just navigate to your favourite place to install things and run the following commands (but remember to change to path to that of your own i3 config): 
+
+```
+git clone https://github.com/nkreer/i3mag
+cd i3mag
+php mag.php install /path/to/your/i3/config
+```
+
+It'll create the shortcuts ``$mod+plus`` for zooming in and ``$mod+minus`` for zooming out. 
---
+++ mag.php
@@ -0,0 +1,53 @@
+<?php
+
+// Simple screen magnification for i3 using xrandr
+
+$command = $argv[1];
+$scriptlocation = __FILE__;
+
+switch($command){
+    case 'install':
+        // Add the necessary configuration to i3 automatically
+        if(!empty($argv[2])){
+            $configpath = $argv[2];
+        } else {
+            echo "Please supply the path to your i3 config.\n";
+            exit();
+        }
+
+        echo "Automatically configuring i3...\n";
+        $prompt = getUserInput("This is going to mess with your i3 configuration in ".$configpath.". Proceed with caution! Would you like to continue? (y/N)");
+
+        // Proceed with installation
+        if(strtolower($prompt) === "y"){
+            $i3config = file_get_contents($configpath);
+            $i3config .= "\n# Auto-configuration for i3mag\n";
+            // Add mod and - for decreasing magnification level
+            $i3config .= 'bindsym $mod+minus exec php '.$scriptlocation.' -'."\n";
+            // Add mod and + for increasing magnification level
+            $i3config .= 'bindsym $mod+plus exec php '.$scriptlocation.' +'."\n";
+            file_put_contents($configpath, $i3config);
+            echo "Configured i3mag.\n";
+        } else {
+            echo "Cancelled.";
+            exit();
+        }
+        break;
+    case '+':
+        // Zoom in
+        shell_exec("xrandr --output LVDS-1 --panning 1360x768 --mode 864x486");
+        break;
+    case '-':
+        // Zoom out
+        shell_exec("xrandr --output LVDS-1 --panning 1360x768 --mode 1360x768");
+        break;
+    default:
+            echo "Invalid command.\n";
+            exit();
+        break;
+}
+
+function getUserInput($prompt){
+    echo $prompt.": ";
+    return trim(fgets(STDIN));
+}
